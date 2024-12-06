c1b49342200069e728a62674adff1e6eebd8101c
Reinhad Tangguh Ariyanto <85008633+rta0227@users.noreply.github.com>
2024-12-06 08:17:04 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes/detectionRoutes");
+const loadModel = require('../services/loadModel');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: "localhost",
+        port: process.env.PORT || 3000,
+        host: "0.0.0.0",
         routes: {
             cors: {
                 origin: ["*"], // Izinkan CORS
             },
         },
-    });
+    });;
+ 
+    const model = await loadModel();
+    server.app.model = model;
 
     // Daftarkan route
     server.route(routes);
